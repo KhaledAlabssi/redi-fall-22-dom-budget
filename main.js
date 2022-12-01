896109454973a7c6abf99b64a230de95edee3b5a
KhaledAlabssi <khaled.absi5@gmail.com>
2022-12-01 23:57:22 +0100

    finalize the first version of the app by adding income and expenses functions

--- main.js
+++ main.js
@@ -1,10 +1,26 @@
-const balance = document.getElementById("balance")
-const totalIncome = document.getElementById('total-income')
-const totalExpenses = document.getElementById('total-expenses')
+const balanceScreen = document.getElementById("balance")
+const totalIncomeScreen = document.getElementById('total-income')
+const totalExpensesScreen = document.getElementById('total-expenses')
+const logList = document.getElementById('log-section')
+let balance = 0;
+let income = 0;
+let expenses = 0;
 // income form
 const incomeForm = document.getElementById('income-form')
 const incomeText = document.getElementById('income-text')
 const incomeAmount = document.getElementById('income-amount')
+// expenses from
+const expensesFrom = document.getElementById('expenses-form')
+const expensesText = document.getElementById('expenses-text')
+const expensesAmount = document.getElementById('expenses-amount')
+
+
+function getBalance() {
+    balance = income - expenses
+    totalIncomeScreen.textContent = income
+    totalExpensesScreen.textContent = expenses
+    balanceScreen.textContent = balance
+}
 
 incomeForm.addEventListener('submit', function (e) {
     e.preventDefault()
@@ -18,9 +34,52 @@ incomeForm.addEventListener('submit', function (e) {
         
     } else {
         alert("Invalid Inputs")
+        incomeAmount.value = ""
+        incomeText.value = ""
         return
     }
-    totalIncome.textContent = amount
+    income += Number(amount)
+    getBalance()
+    incomeLog(amount, text)
 })
 
+expensesFrom.addEventListener("submit", function (e) {
+  e.preventDefault()
+  let text
+  let amount
+  if (expensesText.value.length > 0 && expensesAmount.value > 0) {
+    text = expensesText.value
+    amount = expensesAmount.value
+    expensesAmount.value = ""
+    expensesText.value = ""
+  } else {
+    alert("Invalid Inputs")
+    expensesAmount.value = ""
+    expensesText.value = ""
+    return
+  }
+  expenses += Number(amount)
+    getBalance()
+    expensesLog()
+})
+
+
+function incomeLog(amount, text) {
+    let newLog = document.createElement('p')
+    newLog.classList.add('log-inc')
+    newLog.innerHTML = `
+    <span>${amount}</span> ${text}<i class="fa-solid fa-trash"></i>
+    `
+    logList.insertBefore(newLog, logList.firstChild)
+    
+}
+
+function expensesLog(amount, text) {
+  let newLog = document.createElement("p")
+  newLog.classList.add("log-exp")
+  newLog.innerHTML = `
+    <span>${amount}</span> ${text}<i class="fa-solid fa-trash"></i>
+    `
+  logList.insertBefore(newLog, logList.firstChild)
+}
 
